4e848d79e7e17432833b77171300117767bf82a1
rhectoroc <64383955+rhectoroc@users.noreply.github.com>
2025-11-26 08:53:59 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Este script se ejecuta al iniciar el contenedor.
+# Simplemente llama al comando de inicio configurado en package.json (npm start).
+
+# Ejecuta el servidor Node.js
+npm start
